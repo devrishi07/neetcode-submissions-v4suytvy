052c8a124e5ad3978c890b57ef7ef5b78531b228
devrishi07 <v4448759@gmail.com>
2026-05-11 22:21:36 +0530

    Add: next-greater-element-i - submission-1

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        hashmap = {}
+        stack = []
+
+        for num in nums2:
+            while stack and num > stack[-1]:
+                hashmap[stack.pop()] = num
+            
+            stack.append(num)
+
+        res = [] 
+        for num in nums1:
+            res.append(hashmap.get(num, -1))
+        
+        return res
+        
+        
+                
