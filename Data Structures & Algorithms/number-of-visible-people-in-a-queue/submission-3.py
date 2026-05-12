8b0cf5e79c8a2eb285fade99a383ff63173a12f1
devrishi07 <v4448759@gmail.com>
2026-05-12 14:12:59 +0530

    Add: number-of-visible-people-in-a-queue - submission-3

---
+++ Data Structures & Algorithms/number-of-visible-people-in-a-queue/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canSeePersonsCount(self, heights: List[int]) -> List[int]:
+        n = len(heights)
+        res = [0] * n
+
+
+        for i in range(1, n):
+            x = -1
+            j = i
+            while j:
+                if heights[j - 1] > x:
+                    res[j - 1] += 1
+                    x = heights[j - 1]
+                if x > heights[i]:
+                    break
+                j -= 1
+        
+        return res
+
+
+
+                
+
